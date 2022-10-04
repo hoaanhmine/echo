74cb1942e215840b8e091d0502afccebe4e4ea41
HMFNF <51531073+hoaanhmine@users.noreply.github.com>
2022-10-04 17:23:00 +0700

    stage

--- assets/preload/stages/falling.lua
+++ assets/preload/stages/falling.lua
@@ -1,7 +1,8 @@
 function onCreate()
 	-- background shit
-	makeLuaSprite('backgroundimg', 'whitebg', -700, -500);
+	makeLuaSprite('backgroundimg', 'whitebg', -700, -330);
 	setScrollFactor('backgroundimg', 0, 0);
+	scaleObject('backgroundimg', 50, 50);
 	addLuaSprite('backgroundimg', false);	
 	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
 end
